a9cbf707675387e58076036ee581cad0f3d90501
mayson801 <48494078+mayson801@users.noreply.github.com>
2021-01-19 21:52:43 +0000

    first push

--- spotify_code.py
+++ spotify_code.py
@@ -55,9 +55,8 @@ def get_tracks(auth,artist_id):
             listOfalbum[album['name']]=album
             songs_in_album = auth.album_tracks(album_id=album['id'])
             for song in songs_in_album['items']:
-                if song(['track']['preview_url'] != None):
-                    if song['id'] not in lis_of_songs:
-                        lis_of_songs[song['id']] = {"id":song['id'],"name":song['name'],"preview_url":song['preview_url']}
+                if song['id'] not in lis_of_songs:
+                    lis_of_songs[song['id']] = {"id":song['id'],"name":song['name'],"preview_url":song['preview_url']}
 
     for key in lis_of_songs.keys():
         json_format.append(lis_of_songs[key])
@@ -71,9 +70,8 @@ def get_tracks_for_playlist_or_saved(auth,playlist_id,type):
     else:
         songs = auth.current_user_saved_tracks()
     for song in songs['items']:
-        if song(['track']['preview_url'] != None):
-            if song['track']['id'] not in dict_of_songs:
-                dict_of_songs[song['track']['id']] = {"id": song['track']['id'], "name": song['track']['name'], "preview_url": song['track']['preview_url']}
+        if song['track']['id'] not in dict_of_songs:
+            dict_of_songs[song['track']['id']] = {"id": song['track']['id'], "name": song['track']['name'], "preview_url": song['track']['preview_url']}
 
     for key in dict_of_songs.keys():
         json_format.append(dict_of_songs[key])
